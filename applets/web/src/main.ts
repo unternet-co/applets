24756b2391934f0268d1a8097e5204d485683c25
Rupert <mail@ruperts.world>
2024-12-13 15:12:57 +1100

    Removed dev mode

--- applets/web/src/main.ts
+++ applets/web/src/main.ts
@@ -2,7 +2,7 @@ import { applets } from '@web-applets/sdk';
 import Unternet from '@unternet/sdk';
 
 const context = applets.getContext();
-const unternet = new Unternet({ isDev: true });
+const unternet = new Unternet({ isDev: false });
 
 context.setActionHandler('search', async ({ q }) => {
   const results = await unternet.lookup.query({
