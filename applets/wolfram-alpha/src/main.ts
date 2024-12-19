c9e531804898cfd5076b555fab4b59327923db56
Rupert <mail@ruperts.world>
2024-12-19 21:54:07 +1100

    Add caculator

--- applets/wolfram-alpha/src/main.ts
+++ applets/wolfram-alpha/src/main.ts
@@ -1,48 +1,20 @@
 import { applets } from "@web-applets/sdk";
 
 const context = applets.getContext();
-const appIds = {
-  simple: import.meta.env.VITE_WOLFRAM_SIMPLE_KEY,
-  short_answers: import.meta.env.VITE_WOLFRAM_SHORT_ANSWER_KEY
-};
-const proxyKey = import.meta.env.VITE_PROXY_KEY;
-
-const baseUrl = "http://api.wolframalpha.com/v1";
 
 context.setActionHandler("get_knowledge", async ({ query }) => {
   // Wolfram Alpha short answer API (provides text)
   // https://products.wolframalpha.com/short-answers-api/documentation
-  const textResponse = await fetch(
-    `https://corsproxy.io/?key=${proxyKey}&url=${baseUrl}/result?appid=${appIds.short_answers}&i=${encodeURIComponent(
-      query
-    )}`
-  );
-  const text = await textResponse.text();
+  const response = await fetch(`https://api.unternet.co/lookup/v1/query`, {
+    method: "POST",
+    body: JSON.stringify({
+      q: query,
+      wolfram: true
+    })
+  });
 
-  // Wolfram Alpha simple answer API (provides an image of the result)
-  // https://products.wolframalpha.com/simple-api/documentation
-  const simpleResponse = await fetch(
-    `https://corsproxy.io/?key=${proxyKey}&url=${baseUrl}/simple?appid=${appIds.simple}&i=${encodeURIComponent(query)}`
-  );
-  const imageData = await simpleResponse.arrayBuffer();
-
-  const imageHtml = gifDataToHtmlString(imageData);
+  const { wolfram } = await response.json();
 
   // Final data for the context
-  context.data = { text, imageHtml };
+  context.data = { answer: wolfram.answer };
 });
-
-function gifDataToHtmlString(buffer: ArrayBuffer) {
-  // Convert binary data to base64
-  const base64String = btoa(new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ""));
-
-  // Create the image source URL
-  const gifUrl = `data:image/gif;base64,${base64String}`;
-
-  // Return HTML string
-  return `<img src="${gifUrl}">`;
-}
-
-context.ondata = () => {
-  document.body.innerHTML = context.data.imageHtml ?? "";
-};
