ef0a9431fd905b32922e953f26569b946415168d
Oli Bridgman <oli.bridgman@gmail.com>
2025-01-30 13:40:07 +1300

    feat(RTE): Update RTE editor for appending action
    We need two ways to update the context.

--- src/rich-text-editor/src/main.ts
+++ src/rich-text-editor/src/main.ts
@@ -8,18 +8,15 @@ let editor: Quill | null = null;
 const context = applets.getContext();
 
 context.setActionHandler("write", ({ text }) => {
-  context.data = { text };
+  context.data = { text, source: "applet" };
 });
 
 context.setActionHandler("append", ({ text }) => {
   if (!editor) return;
 
-  const delta = editor.getContents();
-  const content = editor.getText();
-  const html = editor.root.innerHTML;
-  console.log("delta", JSON.stringify(delta));
-  console.log("content", content);
-  console.log("html", html);
+  const currentContent = editor.getSemanticHTML();
+
+  context.data = { text: currentContent + text, source: "applet" };
 });
 
 context.ondata = () => {
@@ -27,7 +24,8 @@ context.ondata = () => {
     throw new Error("No editor found");
   }
 
-  if (context?.data?.text) {
+  if (context?.data?.text && context?.data?.source === "applet") {
+    /** We only want to update the editor if the change came from outside the editor */
     editor.clipboard.dangerouslyPasteHTML(context.data.text);
   }
 };
@@ -37,7 +35,15 @@ context.onload = () => {
   if (!editorElement) {
     throw new Error("No element found for editor");
   }
-  loadEditor(editorElement);
+  const quill = loadEditor(editorElement);
+
+  quill.on("text-change", (_, __, source) => {
+    /** If the change was made by the user, we want to sync the current text content to the applet context */
+    if (source === "user") {
+      /** We're ignoring the deltas and calling `getSemanticHTML`, this might cause performance problems */
+      context.setData({ text: quill.getSemanticHTML(), source: "user" });
+    }
+  });
 };
 
 function loadEditor(element: HTMLElement) {
